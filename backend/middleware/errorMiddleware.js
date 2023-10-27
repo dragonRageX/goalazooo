03adc391bb3a8cbda1580ea83c32823f86e6c7dd
param <param772003@gmail.com>
2023-10-27 18:12:54 +0530

    handling promises back from the db

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,9 +1,7 @@
 const errorHandler = (err, req, res, next) => {
-    // const statusCode = res.statusCode ? res.statusCode : 500;
-    
-    // res.status(statusCode);
+    const statusCode = res.statusCode ? res.statusCode : 500;
 
-    res.json({
+    res.status(statusCode).json({
         message: err.message,
         stack: process.env.NODE_ENV === "production" ? null : err.stack
     });
