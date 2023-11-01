ff3d6b3707f6062bf622299e8804f341cae66cef
param <param772003@gmail.com>
2023-11-01 20:32:51 +0530

    linked/related the goal schema to the user schema

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema(   //think of schemas or models as the layout structure of your collection/table, where you define what fields/columns/attributes you want in your table, and each document/entry will have those fields.
     {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        },
         text: {
             type: String,
             required: [true, "Please add a text value"]
