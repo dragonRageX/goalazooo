19aca7a4711c58a76c0ce0d7c38a1414ea41b892
param <param772003@gmail.com>
2023-11-10 19:11:06 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link
+} from "react-router-dom"
+
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <>
+    <Route path="/" element={<Dashboard />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/register" element={<Register />}></Route>
+  </>
+));
+
 function App() {
 
   return (
-    <>
-      <h1>My App</h1>
-    </>
+    <div className="container">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
