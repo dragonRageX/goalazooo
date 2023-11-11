4f9d46b88178eabf92fccf88cf25c26edb417e45
param <param772003@gmail.com>
2023-11-11 22:32:14 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,21 @@ import {
   Link
 } from "react-router-dom"
 
+import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <>
-    <Route path="/" element={<Dashboard />}></Route>
+  <Route path = "/" element={<Layout />}>
+    <Route index element={<Dashboard />}></Route>
     <Route path="/login" element={<Login />}></Route>
     <Route path="/register" element={<Register />}></Route>
-  </>
+  </Route>
 ));
 
-function App() {
-
+function App()
+{
   return (
     <div className="container">
       <RouterProvider router={router} />
