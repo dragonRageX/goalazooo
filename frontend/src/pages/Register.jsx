642e9e08187a259958e1a67f4f980ac2d74ea78a
param <param772003@gmail.com>
2023-11-11 23:47:57 +0530

    register form functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,89 @@
 import React from "react"
+import { FaUser } from "react-icons/fa"
 
 export default function Register()
 {
+    let [formData, setFormData] = React.useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+
+    function handleChange(e)
+    {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [e.target.name]: [e.target.value]
+        }));
+    }
+
+    function handleSubmit(e)
+    {
+        e.preventDefault();
+    }
+
     return (
-        <h1>Register Page goes here.</h1>
+        <>
+            <section className="heading">
+                <h1>
+                    <FaUser /> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+            <section className="form">
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <div className="form-group">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="name" 
+                            name="name" 
+                            value={formData.name} 
+                            placeholder="Enter your name" 
+                            onChange={(e) => handleChange(e)} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            id="email" 
+                            name="email"
+                            value={formData.email} 
+                            placeholder="Enter your email" 
+                            onChange={(e) => handleChange(e)} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            id="password" 
+                            name="password" 
+                            value={formData.password} 
+                            placeholder="Enter password" 
+                            onChange={(e) => handleChange(e)} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            id="password2" 
+                            name="password2" 
+                            value={formData.password2} 
+                            placeholder="Confirm Password" 
+                            onChange={(e) => handleChange(e)} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-block">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 }
