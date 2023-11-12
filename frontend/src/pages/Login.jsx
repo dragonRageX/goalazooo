fc14afc07c330cfe8a76b19d129d4d1c71434790
param <param772003@gmail.com>
2023-11-12 12:04:12 +0530

    added login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,65 @@
 import React from "react"
+import { FaSignInAlt } from "react-icons/fa"
 
 export default function Login()
 {
+    let [formData, setFormData] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    function handleChange(e)
+    {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [e.target.name]: [e.target.value]
+        }));
+    }
+
+    function handleSubmit(e)
+    {
+        e.preventDefault();
+    }
+
     return (
-        <h1>Login Page goes here.</h1>
+        <>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt /> Login
+                </h1>
+                <p>Login and start setting your Goals!</p>
+            </section>
+            <section className="form">
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <div className="form-group">
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            id="email" 
+                            name="email"
+                            value={formData.email} 
+                            placeholder="Enter your email" 
+                            onChange={(e) => handleChange(e)} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            id="password" 
+                            name="password" 
+                            value={formData.password} 
+                            placeholder="Enter password" 
+                            onChange={(e) => handleChange(e)} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-block">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 }
